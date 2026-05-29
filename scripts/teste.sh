3f64884d0a93b7d5705d21bf09bca1e81a52cabc
biabc <biabcarrascal@gmail.com>
2026-05-29 11:35:59 +0100

    adicionei pasta dos scripts

---
+++ scripts/teste.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Ficheiro: teste.sh
+# Autor : O Meu Nome
+# Descricao: Script echo
+echo "O meu primeiro script"
+
+# Comandos da questão 30 
+cat ../prog.c > copia_script
+echo "DEAPC 202x" >> copia_script
+cat copia_script
